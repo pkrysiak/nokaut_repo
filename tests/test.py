645bd8e8985fd0aec7f8a3b98cee3ba77d337cb3
pkrysiak <pawelkrysiak90@gmail.com>
2013-07-05 13:42:50 +0200

    zmiany

--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,25 @@
 # -*- coding: UTF-8 -*-
 
-import unittest,urllib2,StringIO
+import unittest,urllib2
 from mock import patch
+from responses import *
 from nokaut import lib
-
-# item = wóda
-NOKAUT_RESPONSE = """\
-"""
-# item = 'a'
-NOKAUT_NO_ITEM_RESPONSE = '''/<success>
-<items></items>
-<total>0</total>
-</success>'''
-
-#  key = 1
-NOKAUT_WRONG_KEY_RESPONSE = '''/<fail>
-<code>101</code>
-<message>Invalid API Key</message>
-</fail>'''
-
+from StringIO import StringIO
 
 class NokautExceptionsTest(unittest.TestCase):
 
     def setUp(self):
         self.nokaut_key = 'a8839b1180ea00fa1cf7c6b74ca01bb5'
-        self.nokaut = None
 
     def tearDown(self):
         self.nokaut_key = None
-        self.nokaut = None
 
-    def test_success(self):
+    @patch('nokaut.lib.urllib2')
+    def test_success(self, urllib2):
+        urllib2.urlopen().return_value = NOKAUT_CORRECT_RESPONSE
+        url, price = lib.nokaut_api('rakieta kosmiczna', self.nokaut_key)
+
+    def test_wrong_key_exception(self):
         pass
 
     def test_no_connection_exception(self):
@@ -38,6 +27,3 @@ class NokautExceptionsTest(unittest.TestCase):
 
     def test_no_key_exception(self):
         pass
-
-if __name__ == "__main__":
-    unittest.main()
