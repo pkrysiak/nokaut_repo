6059d018526aef6994e3299b5ec8458c23e380aa
pkrysiak <pawelkrysiak90@gmail.com>
2013-07-05 08:26:04 +0200

    ostatni stan z wczoraj

--- nokaut/lib.py
+++ nokaut/lib.py
@@ -1,29 +1,32 @@
 import urllib
 from lxml import etree
-from StringIO import StringIO
-from types import StringType
 
 
 class WrongKeyException(Exception):
-        '''
-                class which handles input error excepton
-        '''
+
+        def __init__(self,str):
+            self.str = str
+
         def __str__(self):
-                return repr("Wrong key, Can't get results...")
+                return repr(self.str)
+
+class NoKeyException(Exception):
+    pass
+
+class NoConnectionException(Exception):
+    pass
+
 
 def nokaut_api(p_name, key):
-    if not isinstance(key, StringType):
-        raise TypeError
     question = 'http://api.nokaut.pl/?format=xml&key='+key+'&method=nokaut.product.getByKeyword&keyword='+p_name
     response = urllib.URLopener().open(question)
-    xml = response.read()
-    context = etree.parse(StringIO(xml))
+    context = etree.parse(response)
     product_list = context.xpath('//name//text()')
     min_prices = context.xpath('//price_min//text()')
     # print (product_list,min_prices)
     if not product_list:
-        raise WrongKeyException
-    return (product_list[0],min_prices[0])
+        raise WrongKeyException('Wrong key, Can\'t get results...')
+    return product_list[0], min_prices[0]
 
 
 # print nokaut_api('3310', 'a8839b1180ea00fa1cf7c6b74ca01bb5')
--- nokaut/scripts.py
+++ nokaut/scripts.py
@@ -1,39 +1,21 @@
 # -*- coding:UTF-8 -*-
-
-import getopt,sys,lib
+import getopt,sys,lib,argparse
 from lib import WrongKeyException
 
+parser = argparse.ArgumentParser(description='Nokaut lowest price getter.')
+parser.add_argument('-k', help='nokaut personal key')
+parser.add_argument('arg')
 
-def parse_args():
-    key = False
-    args = sys.argv[1:]
-    try:
-        optlist, arg = getopt.getopt(args,'k:')
-        # print optlist,arg
-
-        if not arg:
-            sys.exit('No search argument specified...')
-        elif len(arg) > 1:
-            sys.exit('To many arguments specified...')
-
-        for option,argument in optlist:
-            if option == '-k':
-                key = argument
-
-        if not key:
-            sys.exit('No key specified..')
-
-        return (arg[0],key)
-
-    except getopt.GetoptError:
-        sys.exit(' wrong inpunt, example input: \n scripts.py -k [key] [product name]')
-
+def parse1_args():
+    return vars(parser.parse_args())
 
 def main():
-    arg,key= parse_args()
+    arg, key= parse1_args()['arg'], parse1_args()['k']
     try:
         res = lib.nokaut_api(arg,key)
-        print 'Najtańszy przedmiot: '+res[0]+' Cena: '+res[1]
+        print 'Najtańszy przedmiot: ' + res[0] + ' Cena: '+res[1]
     except WrongKeyException:
         sys.exit('Wrong key specified, can\'t get results')
-main()
+
+
+# main()
